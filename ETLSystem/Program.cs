b981ce19a6f01cde18881a1756f066082ddb22e3
Mykhailo Trufyn <mishathrufyn@gmail.com>
2023-11-01 17:01:16 +0200

    pick up together

--- ETLSystem/Program.cs
+++ ETLSystem/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace ETLSystem
+﻿using ETLSystem.Csv;
+using ETLSystem.DataHandlers;
+using ETLSystem.Repositories;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace ETLSystem
 {
     public class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var csvPath = configuration.GetRequiredSection("CsvPath").Value!;
+            var duplicatePath = configuration.GetRequiredSection("DuplicatePath").Value!;
+            var connectionString = configuration.GetConnectionString("ConnectionString")!;
+
+            var csv = new TaxiTripCsvReader(csvPath);
+
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var readed = csv.ReadAll();
+
+            var handler = new TaxiTripDataHandler(new TaxiTripCsvWriter(duplicatePath));
+            var handled = (await handler.HandleAsync(readed)).ToList();
+
+            var repository = new TaxiTripRepository(connectionString);
+            repository.BulkInsert(handled);
+
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.ElapsedMilliseconds);
         }
     }
 }
